6db1ca49d967f30eb46c7643c21de16b2ff34b39
sayonnara <sayonnara.ns@gmail.com>
2022-03-13 04:20:47 -0300

    First commit

---
+++ R/soma.R
@@ -0,0 +1,14 @@
+#' Função para somar
+#'
+#'Está funcção serve para somar dois número
+#'
+#'@parametro a e b
+#'
+#'@exemplo
+#'somar (a,b)
+#'
+
+ #'@export
+soma <- function(a, b){
+  a+b
+}
---
+++ R/subtrair.R
@@ -0,0 +1,15 @@
+#' Função para subtrair
+#'
+#'Está funcção serve para somar dois número
+#'
+#'@parametro a e b
+#'
+#'@exemplo
+#'subtrair  (a,b)
+#'
+
+#'@export
+
+subtrair <- function (x, y){
+  x-y
+}
